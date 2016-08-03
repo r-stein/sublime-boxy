2ebfd0b0ef53215551e7ac62ff3b6a7b860e9492
Ihor Oleksandrov <io@oivva.com>
2016-08-03 14:48:16 +0300

    feat: add theme configurator
    Closes #97

---
+++ boxy_links.py
@@ -0,0 +1,33 @@
+"""Boxy Theme Links."""
+import sublime
+import sublime_plugin
+import webbrowser
+
+class BoxyChangelogCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		webbrowser.open_new_tab('https://github.com/oivva/boxy/blob/master/CHANGELOG.md')
+
+class BoxyDocsCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		webbrowser.open_new_tab('https://github.com/oivva/boxy/wiki')
+
+class BoxyDonateCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		webbrowser.open_new_tab('https://github.com/oivva/boxy#share-the-love')
+
+class BoxyForumCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		webbrowser.open_new_tab('https://forum.sublimetext.com/t/boxy-the-most-hackable-theme-for-sublime-text-3/20564')
+
+class BoxyIssuesCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		webbrowser.open_new_tab('https://github.com/oivva/boxy/issues')
+
+class BoxyPackageControlCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		webbrowser.open_new_tab('https://packagecontrol.io/packages/Boxy%20Theme')
+
+class BoxyRepoCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		webbrowser.open_new_tab('https://github.com/oivva/boxy')
+
